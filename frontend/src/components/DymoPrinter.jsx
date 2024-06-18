4fe627de2edb368cb8e60fb6ac8f9ff8624de916
Sabra Funderburg <sabra@funderburg.net>
2024-06-18 11:13:30 -0500

    Printing updates

--- frontend/src/components/DymoPrinter.jsx
+++ frontend/src/components/DymoPrinter.jsx
@@ -52,7 +52,7 @@ const sendLabelsToPrinter = async (labelData, type) => {
 
 async function loadLabelTemplate(filename) {
     try {
-        const response = await axios.get(`${REACT_APP_API_URL}${filename}`, {
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}${filename}`, {
             headers: {
                 'Accept': 'application/xml'
             },
