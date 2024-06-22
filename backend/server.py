2334a0b740255436f1293a3d6467c87764376fdd
Sabra Funderburg <sabra@funderburg.net>
2024-06-22 16:26:44 -0500

    Add local env variables

--- backend/server.py
+++ backend/server.py
@@ -6,6 +6,12 @@ from datetime import datetime
 import re
 from supabase import create_client
 from flask_cors import CORS
+""" 
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv() 
+"""
 
 app = Flask(__name__)
 CORS(app, origins='*')
